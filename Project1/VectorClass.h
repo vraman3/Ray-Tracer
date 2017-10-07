aaa86e7bd94b4cafbf6e39f7885049c46885c109
vraman3 <vishwanath.raman3@gmail.com>
2017-10-07 16:51:07 -0400

    Split cpp and header file. Added exception to operator[] overload

--- Project1/VectorClass.h
+++ Project1/VectorClass.h
@@ -1,6 +1,14 @@
+/**
+RayTracing, VectorClass.h
+Custom class for direction vector
+
+@author: Vishwanath Raman
+@version: 1.0 Oct/07/2017
+
+*/
+
 #pragma once
 
-//Author: Vishwanath Raman
 #include <math.h>
 //#include "MatrixClass.h"
 
@@ -39,112 +47,3 @@ public:
 //VectorClass::VectorClass(MatrixClass m) : x(m[0][0] / m[3][0]), y(m[1][0] / m[3][0]), z(m[2][0] / m[3][0])
 //{}
 
-double VectorClass::operator[](const int dimension) const
-{
-	switch (dimension)
-	{
-		case 0: return x;
-				break;
-
-		case 1: return y;
-				break;
-
-		case 2: return z;
-				break;
-
-		default: return NULL;
-				break;
-	}
-}
-
-VectorClass VectorClass::operator+(const VectorClass& b) const
-{
-	return VectorClass(this->x + b.x, this->y + b.y, this->z + b.z);
-}
-
-VectorClass VectorClass::operator-(const VectorClass& b) const
-{
-	return VectorClass(this->x - b.x, this->y - b.y, this->z - b.z);
-}
-
-VectorClass VectorClass::operator*(const VectorClass& b) const
-{
-	return VectorClass(this->x * b.x, this->y * b.y, this->z * b.z);
-}
-
-VectorClass VectorClass::operator*(const double b) const
-{
-	return VectorClass(this->x * b, this->y * b, this->z * b);
-}
-
-VectorClass VectorClass::operator/(const double b) const
-{
-	return VectorClass(this->x / b, this->y / b, this->z / b);
-}
-
-VectorClass::VectorClass()
-{
-	x = 0.0;
-	y = 0.0;
-	z = 0.0;
-}
-
-VectorClass::VectorClass(double i, double j, double k)
-{
-	x = i;
-	y = j;
-	z = k;
-}
-
-double VectorClass::GetX()
-{
-	return x;
-}
-
-double VectorClass::GetY()
-{
-	return y;
-}
-
-double VectorClass::GetZ()
-{
-	return z;
-}
-
-void VectorClass::SetX(double val)
-{
-	x = val;
-}
-
-void VectorClass::SetY(double val)
-{
-	y = val;
-}
-
-void VectorClass::SetZ(double val)
-{
-	z = val;
-}
-
-
-double VectorClass::Magnitude()
-{
-	double temp = x*x + y*y + z*z;
-	return sqrt(temp);
-	
-}
-double VectorClass::DotProd(VectorClass temp)
-{
-	return x * temp.GetX() + y * temp.GetY() + z * temp.GetZ();
-}
-
-VectorClass VectorClass::CrossProd(VectorClass temp)
-{
-	return VectorClass(y*temp.GetZ() - z*temp.GetY(), z*temp.GetX() - x*temp.GetZ(), x*temp.GetY() - y*temp.GetX());
-}
-
-VectorClass VectorClass::Normalize()
-{
-	double value = sqrt(x*x + y*y + z*z);
-	return VectorClass(x / value, y / value, z / value);
-}
