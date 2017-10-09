d436e435c913da803725ee53802dadc0b17eb019
vraman3 <vishwanath.raman3@gmail.com>
2017-10-09 15:30:14 -0400

    Commented TextureMapping class. Need to update GetIllumination() comments in description.

--- Project1/TextureMapping.cpp
+++ Project1/TextureMapping.cpp
@@ -1,12 +1,42 @@
+/**
+	RayTracing, TextureMapping.cpp
+	Class file for Texture Mapping.
+
+	@author: Vishwanath Raman
+	@version: 1.0 Oct/09/2017
+
+*/
+
 #include "TextureMapping.h"
 
-TextureMapping::TextureMapping(double inu, double inv)
+/**
+	Parameterized constructor for TextureMapping class.
+
+	@param paramU: Input parameter for 'u'.
+	@param paramV: Input parameter for 'v'.
+*/
+TextureMapping::TextureMapping(double paramU, double paramV)
 {
-	u = inu;
-	v = inv;
+	u = paramU;
+	v = paramV;
 }
 
-ColourClass TextureMapping::GetIllumination(VectorClass pi, RayClass ray, VectorClass N, VectorClass L, VectorClass V, ColourClass objColour, ColourClass pointCol, int position)
+/**
+	INCOMPLETE
+	Implementation of the GetIllumination virtual class for the TextureMapping class.
+
+	@param pi: The point of intersection.
+	@param ray: The current ray.
+	@param n: The normal to the current point of interseciton.
+	@param l:
+	@param v:
+	@param objColour: The colour of the current object.
+	@param pointColour:
+	@param position:
+
+	@return the colour of the  current point of intersection as a ColourClass.
+*/
+ColourClass TextureMapping::GetIllumination(VectorClass pi, RayClass ray, VectorClass n, VectorClass l, VectorClass v, ColourClass objColour, ColourClass pointCol, int position)
 {
 	return ColourClass();
 }
--- Project1/TextureMapping.h
+++ Project1/TextureMapping.h
@@ -1,6 +1,14 @@
+/**
+	RayTracing, TextureMapping.h
+	Header file for TextureMapping class.
+
+	@author: Vishwanath Raman
+	@version: 1.0 Oct/09/2017
+
+*/
+
 #pragma once
 
-//Author: Vishwanath Raman
 #include "IlluminationClass.h"
 #include "ColourClass.h"
 #include "VectorClass.h"
@@ -12,7 +20,10 @@ class TextureMapping :public IlluminationClass
 		double u, v;
 
 	public:
+		/* Constructor */
 		TextureMapping(double, double);
+
+		/* Methods */
 		ColourClass GetIllumination(VectorClass, RayClass, VectorClass, VectorClass, VectorClass, ColourClass, ColourClass, int);
 };
 
