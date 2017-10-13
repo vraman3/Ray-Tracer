abf2d5aa8f929dff4ba0d85be6419ac0d393cdc2
vraman3 <vishwanath.raman3@gmail.com>
2017-10-13 00:47:06 -0400

    Commented IntersectionInfo.h which has the struct intersectionInfo.

--- Project1/IntersectionInfo.h
+++ Project1/IntersectionInfo.h
@@ -1,3 +1,11 @@
+/**
+	RayTracing, IntersectionInfo.h
+	Header file to implement the intersectionInfo struct.
+
+	@author: Vishwanath Raman
+	@version: 1.0 Oct/13/2017
+
+*/
 #pragma once
 
 #include "TriangleClass.h"
