72dd4cc6d38a45cf7a09a9b176f3751689ede776
vraman3 <vishwanath.raman3@gmail.com>
2017-10-13 02:16:24 -0400

    Commented MatrixClass.h and cpp.
    Check and finish operator overloading for [], for double dimensions.
    
    Check the toVector function and describe in comments.

--- Project1/MatrixClass.cpp
+++ Project1/MatrixClass.cpp
@@ -1,19 +1,41 @@
+/**
+	RayTracing, MatrixClass.cpp
+	Implement a class for matrix storage and operations.
+
+	@author: Vishwanath Raman
+	@version: 1.0 Oct/13/2017
+
+*/
 #include "MatrixClass.h"
 
-MatrixClass::MatrixClass(int r, int c) : mRows(r), mCols(c), m(std::vector<std::vector<double> >(r, std::vector<double>(c, 0.0)))
+/**
+	Default Constructor.
+*/
+MatrixClass::MatrixClass(int paramRows, int paramColumns) : mRows(paramRows), mCols(paramColumns), m(std::vector<std::vector<double> >(paramRows, std::vector<double>(paramColumns, 0.0)))
 {}
 
-MatrixClass::MatrixClass(VectorClass v) : m(std::vector<std::vector<double> >(4, std::vector<double>(1, 1.0))), mRows(4), mCols(1)
+/**
+	Parameterized Constructor.
+*/
+MatrixClass::MatrixClass(VectorClass paramInputVector) : m(std::vector<std::vector<double> >(4, std::vector<double>(1, 1.0))), mRows(4), mCols(1)
 {
-	m[0][0] = v.getX();
-	m[1][0] = v.getY();
-	m[2][0] = v.getZ();
+	m[0][0] = paramInputVector.getX();
+	m[1][0] = paramInputVector.getY();
+	m[2][0] = paramInputVector.getZ();
 }
 
-std::vector<double>& MatrixClass::operator[](int i)
+/**
+	INCOMPLETE TEST FOR MULTIPLE DIMENSIONS
+	Operator Overloading for getting the dimensions of the matrix.
+	( [] )
+
+	@param paramDimension: The dimension of the element to be returned, 0,1,2.
+	@return the element for the dimension specified
+*/
+std::vector<double>& MatrixClass::operator[](int paramDimension)
 {
 	//assert(i >= 0 && i < mRows);
-	return m[i];
+	return m[paramDimension];
 }
 //double& MatrixClass::operator()(int i, int j)
 //{
@@ -25,12 +47,18 @@ std::vector<double>& MatrixClass::operator[](int i)
 //	return m[i * mCols + j];
 //}
 
-MatrixClass MatrixClass::identity(int dimension)
-{
-	MatrixClass idt(dimension, dimension);
+/**
+	Create an identity matrix.
 
-	for (int i = 0; i < dimension; i++)
-		for (int j = 0; j < dimension; j++)
+	@param paramSize: The size of the identity matrix.
+	@return the identity matrix.
+*/
+MatrixClass MatrixClass::identity(int paramSize)
+{
+	MatrixClass idt(paramSize, paramSize);
+
+	for (int i = 0; i < paramSize; i++)
+		for (int j = 0; j < paramSize; j++)
 		{
 		idt.m[i][j] = ((i == j) ? 1.0 : 0.0);
 		}
@@ -38,25 +66,36 @@ MatrixClass MatrixClass::identity(int dimension)
 	return idt;
 }
 
-MatrixClass MatrixClass::operator*(const MatrixClass& a)
-{
-	assert(mCols == a.mRows);
+/**
+	Operator overloading to multiple two matrices.
 
-	MatrixClass result(mRows, a.mCols);
+	@param paramB: The matrix to be multipled with.
+	@return the result of multiplication of the two matrices.
+*/
+MatrixClass MatrixClass::operator*(const MatrixClass& paramB)
+{
+	assert(mCols == paramB.mRows);
+
+	MatrixClass result(mRows, paramB.mCols);
 
 	for (int i = 0; i < mRows; i++)
-		for (int j = 0; j < a.mCols; j++)
+		for (int j = 0; j < paramB.mCols; j++)
 		{
 		result.m[i][j] = 0.0;
 		for (int k = 0; k < mCols; k++)
 		{
-			result.m[i][j] += m[i][k] * a.m[k][j];
+			result.m[i][j] += m[i][k] * paramB.m[k][j];
 		}
 		}
 
 	return result;
 }
 
+/**
+	Create the tranpose of current matrix.
+
+	@return the tranpose of the current matrix.
+*/
 MatrixClass MatrixClass::transpose()
 {
 	MatrixClass result(mCols, mRows);
@@ -68,6 +107,9 @@ MatrixClass MatrixClass::transpose()
 	return result;
 }
 
+/**
+	INCOMPLETE
+*/
 VectorClass MatrixClass::toVector()
 {
 	return VectorClass(m[0][0] / m[3][0], m[1][0] / m[3][0], m[2][0] / m[3][0]);
--- Project1/MatrixClass.h
+++ Project1/MatrixClass.h
@@ -1,3 +1,11 @@
+/**
+	RayTracing, MatrixClass.h
+	Header file to implement MatrixClass.
+
+	@author: Vishwanath Raman
+	@version: 1.0 Oct/13/2017
+
+*/
 #pragma once
 
 #include <cmath>
@@ -6,19 +14,22 @@
 #include <iostream>
 
 #include "VectorClass.h"
-class MatrixClass
+
+ class MatrixClass
 {
 		int mRows, mCols;
 		std::vector<std::vector<double>> m;
 
 	public:
-				
+		/* Constructors */
 		MatrixClass(int, int);
 		MatrixClass(VectorClass);
-				
+		
+		/* Operator Overloading */
 		MatrixClass operator*(const MatrixClass& a);
 		std::vector<double>& operator[](int i);
 
+		/* Methods */
 		static MatrixClass identity(int);
 		MatrixClass transpose();
 		VectorClass toVector();
