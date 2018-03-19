a867c5cf0446f50e09a81954cc68b71f8c232e54
vraman3 <vishwanath.raman3@gmail.com>
2018-03-19 00:35:43 -0400

    Added function prototypes to Transform.h.

--- Project1/Transform.cpp
+++ Project1/Transform.cpp
@@ -9,4 +9,81 @@ Class to implement the different Transformations.
 #pragma once
 
 #include "Transform.h"
+#include "Matrix4x4.h"
+
+Transform::Transform()
+{
+
+}
+
+Transform::Transform(double mat[4][4])
+{
+	m = Matrix4x4(mat[0][0], mat[0][1], mat[0][2], mat[0][3],
+		mat[1][0], mat[1][1], mat[1][2], mat[1][3],
+		mat[2][0], mat[2][1], mat[2][2], mat[2][3],
+		mat[3][0], mat[3][1], mat[3][2], mat[3][3]);
+	mInv = m.Inverse(m);
+}
+
+Transform::Transform(const Matrix4x4 &mat): m(mat), mInv(m.Inverse(mat))
+{
+
+}
+
+Transform::Transform(const Matrix4x4 &mat, const Matrix4x4 matInv)
+{
+
+}
+
+Transform Transform::GetInverse() const
+{
+
+}
+
+Transform Transform::Translate(const VectorClass delta)
+{
+	return Transform();
+}
+
+Transform Transform::Scale(double x, double y, double z)
+{
+	return Transform();
+}
+
+Transform Transform::RotateX(double angle)
+{
+	return Transform();
+}
+
+Transform Transform::RotateY(double angle)
+{
+	return Transform();
+}
+
+Transform Transform::RotateZ(double angle)
+{
+	return Transform();
+}
+
+Transform Transform::Rotate(double angle, const VectorClass& axis)
+{
+	return Transform();
+}
+
+Transform Transform::LookAt(const VectorClass& pos, const VectorClass& look, const VectorClass& up)
+{
+	return Transform();
+}
+
+VectorClass Transform::operator()(const VectorClass& point, bool boolPoint)
+{
+	return VectorClass();
+}
+
+VectorClass Transform::operator()(const VectorClass& vec)
+{
+	return VectorClass();
+}
+
+
 
--- Project1/Transform.h
+++ Project1/Transform.h
@@ -9,15 +9,33 @@ Header file for the Transform class.
 #pragma once
 
 #include "Matrix4x4.h"
+#include "VectorClass.h"
+
 class Transform
 {
 private:
 	/* Variables */
 	Matrix4x4 m, mInv;
-
+	
 public:
 	/* Constructors */
 	Transform();
-	Transform(float mat[4][4]);
+	Transform(double mat[4][4]);
 	Transform(const Matrix4x4 &mat);
+	Transform(const Matrix4x4 &mat, const Matrix4x4 matInv);
+
+	Transform GetInverse() const;
+	Transform Translate(const VectorClass delta);
+	Transform Scale(double x, double y, double z);
+
+	Transform RotateX(double angle);
+	Transform RotateY(double angle);
+	Transform RotateZ(double angle);
+
+	Transform Rotate(double angle, const VectorClass& axis);
+
+	Transform LookAt(const VectorClass& pos, const VectorClass& look, const VectorClass& up);
+
+	VectorClass operator()(const VectorClass& point, bool boolPoint);
+	VectorClass operator()(const VectorClass& vec);
 };
