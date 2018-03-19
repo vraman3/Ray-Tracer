64156ff4897a4f819814ed4264e1de7377b92515
vraman3 <vishwanath.raman3@gmail.com>
2018-03-18 20:12:47 -0400

    Changed Matrix4x4 class to not use Reference (according to new version of pbrt-v2 on github. Commented header and class files for Matrix4x4.

--- Project1/Matrix4x4.cpp
+++ Project1/Matrix4x4.cpp
@@ -8,6 +8,9 @@ Class to implement 4x4 Matrices
 */
 #include "Matrix4x4.h"
 
+/**
+	Default constructor. Identity matrix. Row-major format.
+*/
 Matrix4x4::Matrix4x4()
 {
 	for (int i = 0; i < 4; ++i)
@@ -20,11 +23,25 @@ Matrix4x4::Matrix4x4()
 		}
 }
 
-Matrix4x4::Matrix4x4(float mat[4][4])
+/**
+	Parameterized constructor to take a 4x4 array as input.
+	The resultant Matrix4x4 wil be row-major.
+
+	@param	mat: 4x4 array to be assigned to new Matrix4x4 object.
+*/
+Matrix4x4::Matrix4x4(double mat[4][4])
 {
-	memcpy(m, mat, 16 * sizeof(float));
+	memcpy(m, mat, 16 * sizeof(double));
 }
 
+/**
+	Parameterized constructor to take in 16 double values. Resultant Matrix4x4
+	will be in Row-major format.
+
+	@param	t00-t33: Individual values to be assigned to corresponding elements
+					 of the matrix. t00 refers to row 0 and column 0,
+					 t01 refers to row 0 and column 1, and so on.
+*/
 Matrix4x4::Matrix4x4(double t00, double t01, double t02, double t03, double t10, double t11, double t12, double t13,
 					double t20, double t21, double t22, double t23, double t30, double t31, double t32, double t33)
 {
@@ -34,11 +51,41 @@ Matrix4x4::Matrix4x4(double t00, double t01, double t02, double t03, double t10,
 	m[3][0] = t30;	m[3][1] = t31;	m[3][2] = t32;	m[3][3] = t33;
 }
 
-Matrix4x4* Matrix4x4::Tranpose()
+/**
+	Return the transpose of a given Matrix. Resultant Matrix4x4 will be in
+	row-major format.
+
+	@param	mat: The Matrix4x4 object to be transposed.
+	@return The transposed version of the give Matrix4x4 object, i.e. mat)
+*/
+Matrix4x4 Matrix4x4::Transpose(const Matrix4x4& mat)
 {
-	return new Matrix4x4(
-		m[0][0], m[1][0], m[2][0], m[3][0],
-		m[0][1], m[1][1], m[2][1], m[3][1],
-		m[0][2], m[1][2], m[2][2], m[3][2],
-		m[0][3], m[1][3], m[2][3], m[3][3]);
+	return Matrix4x4(
+		mat.m[0][0], mat.m[1][0], mat.m[2][0], mat.m[3][0],
+		mat.m[0][1], mat.m[1][1], mat.m[2][1], mat.m[3][1],
+		mat.m[0][2], mat.m[1][2], mat.m[2][2], mat.m[3][2],
+		mat.m[0][3], mat.m[1][3], mat.m[2][3], mat.m[3][3]);
+}
+
+/**
+	Return the result of multiplication of two matrices.
+	(m1 is post-multiplied with m2) Resultant Matrix4x4 in row-major format.
+*/
+Matrix4x4 Matrix4x4::Mul(const Matrix4x4& m1, const Matrix4x4& m2)
+{
+	Matrix4x4 result;
+
+	for(int i=0; i<4; i++)
+		for (int j = 0; j < 4; j++)
+		{
+			//								inner product of 	
+			// (i,j)th element of result =  i-th row of m1 multiplied by
+			//								j-th row of m2.
+			result.m[i][j] =	m1.m[i][0] * m2.m[0][j] +
+								m1.m[i][1] * m2.m[1][j] +
+								m1.m[i][2] * m2.m[2][j] +
+								m1.m[i][3] * m2.m[3][j];
+		}
+
+	return result;
 }
--- Project1/Matrix4x4.h
+++ Project1/Matrix4x4.h
@@ -9,6 +9,7 @@ Header file for the Matrix Class.
 #pragma once
 
 #include <iostream>
+#include "Reference.h"
 
 class Matrix4x4
 {
@@ -17,10 +18,11 @@ class Matrix4x4
 public:
 	/* Constructors */
 	Matrix4x4();
-	Matrix4x4(float mat[4][4]);
+	Matrix4x4(double mat[4][4]);
 	Matrix4x4(	double t00, double t01, double t02, double t03, double t10, double t11, double t12, double t13,
 				double t20, double t21, double t22, double t23, double t30, double t31, double t32, double t33);
 
 	/* Functions */
-	Matrix4x4* Tranpose();
+	Matrix4x4 Transpose(const Matrix4x4&);
+	Matrix4x4 Mul(const Matrix4x4& m1, const Matrix4x4& m2);
 };
