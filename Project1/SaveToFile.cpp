363b593a0ec181d03b1f3bc88c3981267cd3f586
vraman3 <vishwanath.raman3@gmail.com>
2022-01-11 02:40:23 +0530

    Added code to display a simple gradient image.
    Top left green to bottom right red. Saved as ppm.

--- Project1/SaveToFile.cpp
+++ Project1/SaveToFile.cpp
@@ -8,7 +8,23 @@ void SaveToFIle::saveppm(const char* filename, int imageWidth, int imageHeight)
 	std::ofstream myfile;
 
 	myfile.open(filename);
-	myfile << "Writing to this file" << std::endl;
+	myfile << "P3\n" << imageWidth << " " << imageHeight << "\n255\n";
+	
+
+	for (int j = imageHeight - 1; j >= 0; --j) {
+		for (int i = 0; i < imageWidth; ++i) {
+			auto r = double(i) / (imageWidth - 1);
+			auto g = double(j) / (imageHeight - 1);
+			auto b = 0.25;
+
+			int ir = static_cast<int>(255.999 * r);
+			int ig = static_cast<int>(255.999 * g);
+			int ib = static_cast<int>(255.999 * b);
+
+			myfile << ir << ' ' << ig << ' ' << ib << '\n';
+		}
+	}
+
 	myfile.close();
 }
 
