2a901f148be72856bd6ea07bb501f6b42ecb0067
vraman3 <vishwanath.raman3@gmail.com>
2022-01-10 18:21:57 +0530

    Fixed typo in the background colour being return in TraceRay_debug and added a comment.

--- Project1/Tracing.cpp
+++ Project1/Tracing.cpp
@@ -412,20 +412,12 @@ ColourClass Tracing::TraceRay_debug(RayClass ray_debug, std::vector<ObjectClass*
 	//auto t_debug = (sphere_debug.GetIntersection(ray_debug));
 	if (closest == -1)
 	{
-		return ColourClass(0., 0.8, 1.0);
+		// This is the background colour.
+		return ColourClass(0.3, 0.8, 1.0);
 	}
 
 	if (currentLowestVal_debug != infinity && currentLowestVal_debug > epsilonval_small)
 	{
-		// Check if this is inefficient. Basically converting a ColourClass to VectorClass
-		// Another option is to set a alias for Colour and Vector so C++ auto converts
-		//return (ColourClass(interRecord_debug.normal) + ColourClass(1.0, 1.0, 1.0)) * 0.5;
-
 		return objects_debug[closest]->GetColour();
 	}
-	
-	//currentLowestVal_debug = 0.5 * (ray_debug.GetRayDirection().normalize().getY() + 1.0);
-
-	//ColourClass pixelColour_debug = ColourClass(1.0, 1.0, 1.0) * (1.0 - t_debug) + ColourClass(0.5, 0.7, 1.0) * t_debug;
-	//return ColourClass(1.0, 1.0, 1.0) * (1.0 - currentLowestVal_debug) + ColourClass(0.5, 0.7, 1.0) * currentLowestVal_debug;
 }
